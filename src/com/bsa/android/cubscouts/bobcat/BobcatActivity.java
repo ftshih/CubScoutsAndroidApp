5e25fc9c7a93ccead11c7dd1530f7b5b2af52c9b
Fangwha Shih <ftshih@cvty.com>
2011-07-28 09:27:48 -0500

    add a comment

--- src/com/bsa/android/cubscouts/bobcat/BobcatActivity.java
+++ src/com/bsa/android/cubscouts/bobcat/BobcatActivity.java
@@ -4,7 +4,9 @@ import com.bsa.android.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/**
+  Display bob cat requirements
+*/
 public class BobcatActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
